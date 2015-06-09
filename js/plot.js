22468c31cbd91bbfb654fb420d21c29ecad1f2e0
Will Parkin <wmparkin@gmail.com>
2015-06-09 10:28:29 -0400

    put citations in highcharts legend

--- js/plot.js
+++ js/plot.js
@@ -49,6 +49,7 @@ var t_2nm_data = generate_thickness_isoline(2, d_dna, 5.950);
 
 var bibtex;
 
+// Refreshes the <sup class="bib-*"></sup> with the correct citations.
 function refresh_bib() {
   if (bibtex == null) {
     return
@@ -58,11 +59,11 @@ function refresh_bib() {
   var citations = $('[class^=bib-]').each(function(i) {
     var citation = $(this);
     citation.empty();
-    var classN = citation[0].className.replace("bib-", "");
-    var index = cited.indexOf(classN);
+    var key = citation[0].className.replace("bib-", "");
+    var index = cited.indexOf(key);
     if (index < 0) {
       citation.append(count + 1);
-      cited.push(classN);
+      cited.push(key);
       count++;
     } else {
       citation.append(index + 1);
@@ -70,8 +71,7 @@ function refresh_bib() {
   });
 }
 
-
-$(document).ready(function() {
+function plotChartAndTable(series) {
 
   // Set the highcharts plot options
   var options = {
@@ -98,7 +98,15 @@ $(document).ready(function() {
       align: 'left',
       verticalAlign: 'bottom',
       padding: 3,
-      borderWidth: 0
+      borderWidth: 0,
+      labelFormatter: function () {
+        var ret = this.name;
+        if(this.options.hasOwnProperty('bib')){
+          ret += '<sup class="bib-' + this.options.bib + '"></sup>';
+        }
+        return ret;
+      },
+      useHTML: true
     },
     series: [{
       type: 'spline',
@@ -150,84 +158,98 @@ $(document).ready(function() {
     }]
   };
 
-  // Grab the json data
-  $.getJSON('data/data.json', function(series) {
-    var tableStuff = [];
-    var temp;
+  var tableStuff = [];
+  var temp;
 
-    // Format the highcharts options as well as the dataTables data
-    for (var i = 0; i < series.length; i++) {
-      temp = series[i];
-      temp.type = 'scatter';
-      temp.marker = {
-        radius: 8
-      }; // Set the marker radius. There must be a global way of doing this.
-      options.series.push(series[i]);
+  // Format the highcharts options as well as the dataTables data
+  for (var i = 0; i < series.length; i++) {
+    temp = series[i];
+    temp.type = 'scatter';
+    temp.marker = {
+      radius: 8
+    }; // Set the marker radius. There must be a global way of doing this.
+    options.series.push(temp);
 
-      // Create the table row. TODO remove if/else, should not need once all the data is collected.
-      var t = [];
-      t.push(series[i].name);
-      // push max, should be first datapoint
-      t.push(series[i].data[0][1]);
-      // push link if exists, otherwise blank
-      if (series[i].hasOwnProperty('link')) {
-        t.push(series[i].link);
-      } else {
-        t.push("");
-      }
-      // push bib if exists, otherwise blank
-      if (series[i].hasOwnProperty('bib')) {
-        t.push(series[i].bib);
-      } else {
-        t.push('');
-      }
-      tableStuff.push(t);
+    // Create the table row. TODO remove if/else, should not need once all the data is collected.
+    var t = [];
+    t.push(series[i].name);
+    // push max delta G, should be first datapoint
+    t.push(series[i].data[0][1]);
+    // push link if exists, otherwise blank
+    if (series[i].hasOwnProperty('link')) {
+      t.push(series[i].link);
+    } else {
+      t.push("");
     }
+    // push bib if exists, otherwise blank
+    if (series[i].hasOwnProperty('bib')) {
+      t.push(series[i].bib);
+    } else {
+      t.push('');
+    }
+    tableStuff.push(t);
+  }
 
-    // Plot the highcharts chart
-    $('#ds-plot').highcharts(options);
+  // Plot the highcharts chart
+  $('#ds-plot').highcharts(options);
 
-    // Add the datatable
-    $('#ds-table').dataTable({
-      "data": tableStuff,
-      "order": [
-        [1, "desc"]
-      ],
-      "columns": [{
-        "title": "Name",
-        // linkify if the link is available
-        "render": function(data, type, row) {
-          var ret = '';
-          if (row[3].length > 0)
-           ret += '<sup class="bib-' + row[3] + '"></sup>';
-          if (row[2].length > 0)
-            return ret + '<a href="' + row[2] + '">' + data + '</a>';
-          return data;
-        },
-      }, {
-        "title": "&Delta;G"
-      }, {
-        "title": "Link",
-        "visible": false
-      }, {
-        "title": "Cite",
-        "visible": false
-      }]
-    });
-
-  }).fail(function(jqxhr, textStatus, error) {
-    var err = textStatus + ", " + error;
-    console.log("Request Failed: " + err);
+  // Add the datatable
+  $('#ds-table').dataTable({
+    "data": tableStuff,
+    "order": [
+      [1, "desc"]
+    ],
+    "columns": [{
+      "title": "Name",
+      // linkify if the link is available
+      "render": function(data, type, row) {
+        var ret = '';
+        if (row[3].length > 0){
+          ret += '<sup class="bib-' + row[3] + '"></sup>';
+        }
+        if (row[2].length > 0){
+          return '<a href="' + row[2] + '">' + data + '</a>' + ret;
+        }
+        return data + ret;
+      },
+    }, {
+      "title": "&Delta;G"
+    }, {
+      "title": "Link",
+      "visible": false
+    }, {
+      "title": "Cite",
+      "visible": false
+    }]
   });
 
+}
+$(document).ready(function() {
+  // Grab the json data
+  var d1 = $.getJSON('data/data.json');
+
   // Load the bibliography
-  $.ajax({
+  var d2 = $.ajax({
     url: "assets/bib.bib",
-    dataType: "text",
-    success: function(data) {
-      bibtex = bibtexParse.toJSON(data);
-      refresh_bib();
+    dataType: "text"
+  });
+
+  // Wait until both the bibliography text and json data are loaded.
+  $.when(d1, d2).then(function(result1, result2) {
+
+    // parse the bibtex
+    var rawBib = result2[0];
+    var bib = bibtexParse.toJSON(rawBib);
+    // convert the bibtex array to a key/value object
+    bibtex = {};
+    for (var i = 0; i < bib.length; i++) {
+      bibtex[bib[i].citationKey] = bib[i];
     }
+
+    var series = result1[0];
+    plotChartAndTable(series);
+    refresh_bib();
+
   });
 
 });
