5ad49fb09426f152f3f52c3cfc758e9d4b4919c8
Will <wmparkin@gmail.com>
2015-07-09 16:15:38 -0400

    use svg icons instead of icon fonts :heart_eyes:

--- tasks/build.js
+++ tasks/build.js
@@ -1,17 +1,18 @@
 'use strict';
 
 var gulp = require('gulp');
+var connect = require('gulp-connect');
 var sass = require('gulp-sass');
 var babel = require('gulp-babel');
 var svgSprite = require('gulp-svg-sprite');
 var path = require('path');
 var del = require('del');
-var connect = require('gulp-connect');
 
 var basePath = path.join(__dirname, '..');
 
 var srcDir = path.join(basePath, 'app');
 var destDir = path.join(basePath, 'build');
+var stylesheetsDir = path.join(destDir, 'stylesheets');
 
 var paths = {
   jsCodeToTranspile: [
@@ -23,16 +24,17 @@ var paths = {
   toCopy: [
     'app/node_modules/**',
     'app/bower_components/**',
-    '!app/bower_components/**/*.svg',
+    '!app/bower_components/octicons',
     'app/**/*.html',
     'app/favicon.ico',
     'app/assets/**/*',
     'app/data/**',
     'app/css/**',
     'app/images/**',
-    'app/vendor/**'
+    'app/vendor/**',
+    '!app/**/*.scss'
   ],
-  svg: [
+  octicons: [
     'app/bower_components/octicons/svg/*.svg'
   ]
 }
@@ -58,26 +60,41 @@ var transpileTask = function() {
 gulp.task('transpile', ['clean'], transpileTask);
 gulp.task('transpile-watch', transpileTask);
 
-var svgTask = function() {
-  return gulp.src(paths.svg)
+var sassTask = function() {
+  return gulp.src('app/stylesheets/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest(stylesheetsDir))
+    .pipe(connect.reload());
+}
+gulp.task('sass', ['clean'], sassTask);
+gulp.task('sass-watch', sassTask);
+
+var octiconTask = function() {
+  return gulp.src(paths.octicons)
     .pipe(svgSprite({
       mode: {
-        defs: true
+        defs: true,
+      },
+      shape: {
+        id: {
+          generator: "octicon-%s"
+        }
       }
     }))
     .pipe(gulp.dest(destDir))
+    .pipe(connect.reload())
 }
-gulp.task('svg', ['clean'], svgTask);
-gulp.task('svg-watch', svgTask);
+gulp.task('octicons', ['clean'], octiconTask);
+gulp.task('octicons-watch', octiconTask);
 
 gulp.task('watch', function() {
   gulp.watch(paths.jsCodeToTranspile, ['transpile-watch']);
   gulp.watch(paths.toCopy, ['copy-watch']);
-  gulp.watch(paths.svg, ['svg-watch']);
-  // TODO Add sass support
+  gulp.watch(paths.octicons, ['octicons-watch']);
+  gulp.watch('app/stylesheets/**/*.scss', ['sass-watch']);
 })
 
-gulp.task('build', ['transpile', 'svg', 'copy']);
+gulp.task('build', ['transpile', 'octicons', 'sass', 'copy']);
 
 gulp.task('connect', ['build', 'watch'], function() {
   connect.server({
